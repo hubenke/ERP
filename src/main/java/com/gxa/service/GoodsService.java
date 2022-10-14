54e8390c6b53659891900d29d07889ea2767aa86
qidonglin <29030100616@qq.com>
2022-10-14 11:51:09 +0800

    拉取实体类

--- src/main/java/com/gxa/service/GoodsService.java
+++ src/main/java/com/gxa/service/GoodsService.java
@@ -5,20 +5,13 @@ import com.gxa.entity.Goods;
 
 import java.util.List;
 
-/**
- * @author:
- * @ClassName:GoodsService
- * @Package:com.gxa.service.GoodsService
- * @Description:
- * @CreateDate:2022年10月12日
- * @Version:
- **/
+
 public interface GoodsService {
-    List<Goods> queryAll(); //查询所有
+    List<Goods> queryGoods(); //查询所有
 
     void add(Goods goods );;//添加商品
 
-    void  queryGoods();//查询商品表
+//    void  queryGoods();//查询商品表
 
     void  update(Goods goods);//修改
 
