b393693e55383b020f927e13630ecdd1fe48b6b5
qidonglin <29030100616@qq.com>
2022-10-14 09:02:57 +0800

    11

--- src/main/java/com/gxa/controller/Test.java
+++ src/main/java/com/gxa/controller/Test.java
@@ -2,6 +2,6 @@ package com.gxa.controller;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("11111111111111111111");
+        System.out.println("11111111111111");
     }
 }
