110e181bc603716dc9d3ac5db84b46adc41c42a6
laiqingying <981491022@qq.com>
2022-10-14 08:57:04 +0800

    cargo实体类

---
+++ src/main/java/com/gxa/entity/Cargo.java
@@ -0,0 +1,24 @@
+package com.gxa.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@ApiModel("货位实体类")
+public class Cargo {
+    @ApiModelProperty("货位id")
+    private Integer cid;
+
+    @ApiModelProperty("货位号")
+    private String location;
+
+    @ApiModelProperty("货区")
+    private String area;
+
+    @ApiModelProperty(value = "仓库",name = "仓库")
+    private Repository repository;
+
+    @ApiModelProperty(value = "商品",name = "商品")
+    private Goods goods;
+}
